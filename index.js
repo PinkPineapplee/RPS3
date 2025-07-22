b2c43f582796b7b184e5becbc1a21cb1a3c07646
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-07-21 21:28:45 -0700

    Game functionalities

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const container= document.querySelector("div");
-
+let turn = X;
 
  const board ={
     array :[2,2,2,2,2,2,2,2,2,2]
 
  }
 
- const player = {
-   player1: X,
-    player2: O,
+ const person = {
+   person1: X,
+    person2: O,
  }
+
+ function gameStart(){};
+ function GameOver(){};
+ function SwitchPlayer(){
+   if (turn === x){
+      player = O;
+      countinuegame();
+   } 
+ };
