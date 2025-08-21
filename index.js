3f2aa35354ac0a0feae4cd091d65f58df1ac1b84
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-08-21 08:38:11 +0100

    Add playGame functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const container= document.querySelector("div");
- let currentPlayer = X ;
+let gameStart= false;
+ let currentPlayer = "X" ;
 
  const board ={
     array :["null","null","null","null","null","null","null","null","null"]
@@ -7,23 +8,36 @@ const container= document.querySelector("div");
  }
 
  const person = {
-   playerOne: X,
-    playerTwo: O,
+   playerOne: "X",
+    playerTwo: "O",
  }
 
- function gameStart(current){
-
+ function playGame(current){
+   gameStart= true;
+  if (current === "X"){
+   console.log("where are you playing X");
+    board.array=person.playerOne;
+  } else if(current ==="O"){
+   console.log("where are you playing O")
+ board.array=person.playerTwo;
+  } 
  };
  function GameOver(){
   board.array ==! "null"? "GAMEOVER": continueGame;
   console.log("GameOver");
  };
  function SwitchPlayer(){
-  
+   
    currentPlayer === X ? "O":"X";
    return currentPlayer
  };
 
  function continueGame(placeHolder){
+    console.log(`it's ${placeHolder}'s turn to Play!`);
+   playGame();
+ }
 
+
+ function eventPropagation(){
+ // add eventListeners for Players
  }
